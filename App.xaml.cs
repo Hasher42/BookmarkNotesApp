b0b41ee487e165a51c5867fd312466a3de29e5bd
Hasher.hanif <hasher.hanif@crossonic.com>
2025-09-10 19:10:02 +0500

    First Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BookmarkNotesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BookmarkNotesApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
